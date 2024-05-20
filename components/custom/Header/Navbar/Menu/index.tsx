08e6fbe5a561244c9913b20d5bfe5290432ccb4b
LMN <m.loulou9311@gmail.com>
2024-05-20 17:23:45 +0200

    Mettre à jour les dépendances et apporter des modifications à l'interface utilisateur

--- components/custom/Header/Navbar/Menu/index.tsx
+++ components/custom/Header/Navbar/Menu/index.tsx
@@ -93,7 +93,9 @@ const Menu = () => {
           </NavigationMenuContent>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <NavigationMenuTrigger className="bg-transparent  ">Projets</NavigationMenuTrigger>
+          <Link href="/projets" passHref>
+            <NavigationMenuTrigger className="bg-transparent  ">Projets</NavigationMenuTrigger>
+          </Link>
           <NavigationMenuContent>
             <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
               {components.map((component) => (
