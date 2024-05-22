10ef6dc5eeb520e68589f560f9ba6795b24ec055
LMN <m.loulou9311@gmail.com>
2024-05-22 17:08:16 +0200

    Mise à jour du composant Logo pour utiliser l'entité HTML &gt; au lieu de la flèche de la fonction =>

--- components/custom/Logo/StaticLogo.tsx
+++ components/custom/Logo/StaticLogo.tsx
@@ -2,7 +2,7 @@ const StaticLogo = () => {
    return (
       <div className="grid grid-flow-row">
          <span className="text-[#4ca5d4]">~</span>
-         <p className="mb-2 font-fira dark:text-[#59f687] text-lg font-medium"> <span className="text-[#f84534]"> => </span>i-code</p>
+         <p className="mb-2 font-fira dark:text-[#59f687] text-lg font-medium"> <span className="text-[#f84534]"> {"=>"} </span>i-code</p>
       </div>
    );
 }
